0b70f1c9d5e7454348a27c00c3489c29a0edaa02
mostafagamal-cell <76878167+mostafagamal-cell@users.noreply.github.com>
2024-09-20 15:28:14 +0300

    add

--- app/src/main/java/com/example/weatherapp/AppViews/MapFragment.kt
+++ app/src/main/java/com/example/weatherapp/AppViews/MapFragment.kt
@@ -32,17 +32,43 @@ class MapFragment : Fragment() {
         db = FragmentMapBinding.inflate(layoutInflater)
         return db.root
     }
-
+    private lateinit var startPoint:GeoPoint
     lateinit var mymap: MapView
      var marker: Marker?=null
+    var res=""
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mymap = db.map
+        startPoint = GeoPoint(48.8583, 2.2944)
+
+        if (savedInstanceState!=null){
+            if (savedInstanceState.containsKey("res")&&savedInstanceState.containsKey("lat")&&savedInstanceState.containsKey("lon")){
+            val la=savedInstanceState.getDouble("lat")
+            val lo=savedInstanceState.getDouble("lon")
+            res=savedInstanceState.getString("res","")
+            startPoint=GeoPoint(la, lo)
+            marker=Marker(mymap)
+        }
+        }
+        requireActivity().getSharedPreferences(map, Context.MODE_PRIVATE).edit().clear().apply()
         super.onViewCreated(view, savedInstanceState)
         val sharedPreferences = requireContext().getSharedPreferences(map, Context.MODE_PRIVATE)
         Configuration.getInstance().load(requireContext(), sharedPreferences)
-        mymap = db.map
         if (marker==null){
             set_invis(db.floatingActionButton2)
             set_invis(db.floatingActionButton3)
+        }else{
+            set_vis(db.floatingActionButton2)
+            set_vis(db.floatingActionButton3)
+            marker?.position = startPoint
+            marker?.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            try {
+                marker?.title = res
+                Toast.makeText(requireContext(), res, Toast.LENGTH_LONG).show()
+            }catch (e:Exception){
+                Toast.makeText(requireContext(), "No connection", Toast.LENGTH_LONG).show()
+
+            }
+            mymap.overlays.add(marker)
         }
         mymap.setScrollableAreaLimitDouble(BoundingBox(85.0, 180.0, -85.0, -180.0))
         mymap.maxZoomLevel = 20.0;
@@ -51,8 +77,7 @@ class MapFragment : Fragment() {
         mymap.setMultiTouchControls(true)
         mymap.isHorizontalMapRepetitionEnabled = false
         mymap.isVerticalMapRepetitionEnabled = false
-        val startPoint = GeoPoint(48.8583, 2.2944)
-        mymap.controller.setZoom(15.0)
+        mymap.controller.setZoom(3.0)
         mymap.controller.setCenter(startPoint)
         mymap.overlays.add(MapEventsOverlay(object : MapEventsReceiver {
             override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
@@ -63,13 +88,12 @@ class MapFragment : Fragment() {
                 val latitude = p.latitude
                 val longitude = p.longitude
                 val geocoder= Geocoder(requireContext())
+
                 mymap.overlays.remove(marker)
                 marker = Marker(mymap)
                 marker?.position = p
                 marker?.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                var res=""
                 try {
-                   val x= geocoder.getFromLocation(latitude,longitude,1)?.get(0)
                     res= geocoder.getFromLocation(latitude,longitude,1)?.get(0)?.getAddressLine(0)?:""
                     marker?.title = res
                     Toast.makeText(requireContext(), res, Toast.LENGTH_LONG).show()
@@ -119,5 +143,14 @@ class MapFragment : Fragment() {
     override fun onPause() {
         super.onPause()
         mymap.onPause() // Needed for osmdroid lifecycle
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (marker!=null){
+            outState.putString("res", marker!!.title)
+            outState.putDouble("lat", marker!!.position.latitude)
+            outState.putDouble("lon", marker!!.position.longitude)
+        }
     }
 }
